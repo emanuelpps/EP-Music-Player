547ce1a9a2722a3225b94b174cd4fc08f954d108
emanuelpps <emanuelpages.ps@gmail.com>
2023-07-30 23:47:46 -0300

    correccion en logica de like button

--- src/Components/Buttons/LikeButton/LikeButton.jsx
+++ src/Components/Buttons/LikeButton/LikeButton.jsx
@@ -1,15 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./LikeButton.css";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { MusicContext } from "../../../Context/MusicContext";
 
 export default function LikeButton() {
-  const { addSongToPlaylist, songInfo, activeLikeButton} =
-    useContext(MusicContext);
-    
+  const { addSongToPlaylist, songInfo, activeLikeButton,userPlaylist, streamSong } =  useContext(MusicContext);
+  const [checkLikeButton, setCheckLikeButton] = useState("");
 
-
+  useEffect(() => {
+    const inPlaylist = userPlaylist.find((track) => track.id === songInfo.id)
+      ? true
+      : false;
+    // Actualizar el estado del botón cada vez que activeLikeButton cambie
+    if (inPlaylist) {
+      setCheckLikeButton(<FaHeart className="likeButtonFill" />);
+    } else {
+      setCheckLikeButton(<FaRegHeart />);
+    }
+  }, [streamSong, activeLikeButton, checkLikeButton]);
 
   return (
     <div>
@@ -17,7 +26,7 @@ export default function LikeButton() {
         className="buttonReset likeButton m-2"
         onClick={() => addSongToPlaylist(songInfo?.id)}
       >
-        {activeLikeButton ? <FaHeart className="likeButtonFill" /> : <FaRegHeart />}
+        {checkLikeButton}
       </button>
     </div>
   );
